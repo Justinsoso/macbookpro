601d5004584b9f9affc25f2bd536bb67999d8239
Justin <justin1995so@gmail.com>
2019-11-21 19:12:32 +0800

    Exercism

---
+++ Exercism/javascript/resistor-color/resistor-color.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const colorCode = (color) => {
+  switch(color){
+    case "Black":
+      return 0;
+      break;
+    case 'Brown':
+      return 1;
+      break;
+    case 'Red':
+      return 2;
+      break;
+  };
+};
+
+export const COLORS = ['Black','Brown','Red'];
